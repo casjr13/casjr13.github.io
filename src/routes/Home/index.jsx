86b28a2806aacaac6d091ce76ce41cb7d9daaed3
Christopher Salinas <casjr13@unm.edu>
2018-06-25 22:08:36 -0600

    feat(home): add gallery

--- src/routes/Home/index.jsx
+++ src/routes/Home/index.jsx
@@ -1,10 +1,32 @@
 import React from 'react';
+import {map} from 'lodash';
 import AppLayout from '../../containers/AppLayout';
 
 import './Home.scss';
 
+const images = [
+  "/images/gallery/20180622-SandiaPark02.jpg",
+  "/images/gallery/20180622-SandiaPark01.jpg",
+  "/images/gallery/20180622-Mustang01.jpg",
+  "/images/gallery/20180618-Mustang01.png",
+  "/images/gallery/20180618-Edgewood01.png",
+  "/images/gallery/20180526-Thunderbirds04.jpg",
+  "/images/gallery/20180526-Thunderbirds03.jpg",
+  "/images/gallery/20180526-Thunderbirds02.jpg",
+  "/images/gallery/20180526-Thunderbirds01.jpg",
+];
+
 class Home extends React.Component {
   render() {
+    const gallery = map(images, (imgPath, i) => {
+      const img = <i style={{backgroundImage: `url(${imgPath})`}}/>;
+      return <a
+        className="img"
+        href={imgPath}
+        key={`image${i}`}
+        target="_blank"
+      >{img}</a>;
+    });
     return (
       <AppLayout className="home">
         <div className="banner">
@@ -19,6 +41,9 @@ class Home extends React.Component {
               <p>I am an amateur photographer that practices on my own time. For photography inquiries, please email <a href="mailto:chrissalinasjr@gmail.com">chrissalinasjr@gmail.com</a>.</p>
             </div>
           </div>
+        </section>
+        <section className="gallery">
+          {gallery}
         </section>
       </AppLayout>
     );
